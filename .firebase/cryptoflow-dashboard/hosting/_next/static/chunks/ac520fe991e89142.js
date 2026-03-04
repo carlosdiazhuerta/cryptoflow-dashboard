afa93a49021f26b1d24952dd524ab057f31bd106
Carlos Francisco Diaz Huerta <carlosfranciscodiazhuerta@MacBook-Air-de-Carlos.local>
2026-03-04 18:50:25 -0500

    globalize state with zustand in secondary views

---
+++ .firebase/cryptoflow-dashboard/hosting/_next/static/chunks/ac520fe991e89142.js
@@ -0,0 +1,1 @@
+(globalThis.TURBOPACK||(globalThis.TURBOPACK=[])).push(["object"==typeof document?document.currentScript:void 0,20558,t=>{"use strict";let a=(0,t.i(39460).default)("chart-column",[["path",{d:"M3 3v16a2 2 0 0 0 2 2h16",key:"c24i48"}],["path",{d:"M18 17V9",key:"2bz60n"}],["path",{d:"M13 17V5",key:"1frdt8"}],["path",{d:"M8 17v-3",key:"17ska0"}]]);t.s(["BarChart3",()=>a],20558)}]);
